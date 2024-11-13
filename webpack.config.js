0a13cc991eb0725e9cb4f7716efd7eada81ef77b
Adrien Patuzzo <a.pats@hotmail.fr>
2024-11-13 01:05:55 +0100

    Nouvelle version du Portfolio Adrien Patuzzo

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const Encore = require('@symfony/webpack-encore');
+
+// Commentons tout temporairement
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    // .addEntry('app', './assets/app.js')  // Commenté
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+;
+
+module.exports = Encore.getWebpackConfig();
